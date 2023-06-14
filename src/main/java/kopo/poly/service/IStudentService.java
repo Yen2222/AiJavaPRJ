f53595ec80bb6924f4a13ce752cb9d49a981d387
data22 <lyebh1234@gmail.com>
2023-06-14 14:28:48 +0900

    2023.06.14

--- src/main/java/kopo/poly/service/IStudentService.java
+++ src/main/java/kopo/poly/service/IStudentService.java
@@ -8,5 +8,9 @@ public interface IStudentService {
 
     List<StudentDTO> insertStudent(StudentDTO pDTO) throws Exception;
 
+    void insertStudentlist(List<StudentDTO> plist) throws Exception;
+
     List<StudentDTO> deleteStudent(StudentDTO pDTO) throws Exception;
+
+    List<StudentDTO> updateStudent(StudentDTO pDTO) throws Exception;
 }
